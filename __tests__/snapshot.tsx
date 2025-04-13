06a706e1bd35d9f05ae94d0f83b544020d7142c5
Raihan <raihanramadhan.rn22@gmail.com>
2025-04-14 03:33:17 +0700

    install jest for unit and snapshot testing

---
+++ __tests__/snapshot.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render } from "@testing-library/react";
+
+import Home from "@/app/page";
+
+it("renders homepage unchanged", () => {
+  const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
+});
